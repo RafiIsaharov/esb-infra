77433adea6016d1722c6efc42174bd41556b6fa4
rafi.isaharov <rafi.isaharov@dh.com>
2024-07-29 00:02:31 +0300

    added HumanReadableTestNames and dependencies related to Junit

---
+++ src/test/java/com/rafi/esb/infra/tools/HumanReadableTestNames.java
@@ -0,0 +1,31 @@
+package com.rafi.esb.infra.tools;
+
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+import java.lang.reflect.Method;
+
+// use via @DisplayNameGeneration(HumanReadableTestNames.class) on a testclass
+public class HumanReadableTestNames extends DisplayNameGenerator.Standard {
+    public HumanReadableTestNames() {
+    }
+
+    public String generateDisplayNameForClass(Class<?> testClass) {
+        return replaceCapitals(super.generateDisplayNameForClass(testClass));
+    }
+
+    public String generateDisplayNameForNestedClass(Class<?> nestedClass) {
+        return replaceCapitals(super.generateDisplayNameForNestedClass(nestedClass));
+    }
+
+    public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+        return replaceCapitals(testMethod.getName());
+    }
+
+    private String replaceCapitals(String name) {
+        name = name.replaceAll("([A-Z])", " $1")
+                .replaceAll("_"," > ")
+                .replaceAll("\\s+"," ")
+                .toLowerCase();
+        return name;
+    }
+}
