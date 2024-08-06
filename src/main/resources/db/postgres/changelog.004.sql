9df46e7aa7083c7ae0abf941f073511b5a21b606
rafi.isaharov <rafi.isaharov@dh.com>
2024-08-06 19:30:14 +0300

    removed  changelog.002.xml and changelog.004.sql

--- src/main/resources/db/postgres/changelog.004.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset rafi.isaharov:6
---comment: supporting second response
-ALTER TABLE routes ADD COLUMN origSecResponse VARCHAR;
-ALTER TABLE routes ADD COLUMN transformedSecResponse VARCHAR;
-ALTER TABLE routes ADD COLUMN secmatchingId VARCHAR;
-
-
